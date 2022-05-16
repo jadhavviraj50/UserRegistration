7ab70a9d27b4af668ba3a334375aba9e43e77538
Viraj <jadhavviraj50@gmail.com>
2022-05-17 00:09:21 +0530

    [Viraj]-Added UC1-FirstNameValidation file

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using UserRegistrationRegex;
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to User Registration Problem");
+
+        Validation validation = new Validation();
+        validation.ValidateFirstName();
+    }
+}
---
+++ UserRegistration/UserRegistration/Validation.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    public class Validation
+    {
+        
+       
+        public void ValidateFirstName()
+        {
+
+            Console.WriteLine("Enter First Name : ");
+            string name = Console.ReadLine();
+
+            string FIRSTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex reg = new Regex(FIRSTNAME_REGEX);
+
+            if (reg.IsMatch(name))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("Verify First Name Again");
+            }
+            
+        }
+    }
+}
